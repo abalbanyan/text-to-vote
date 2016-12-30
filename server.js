d811c590f6169a9561fd39c1f8add1537f785a98
Abdullah <abalbanyan@hotmail.com>
2016-12-30 08:05:53 -0800

    receives SMS and responds

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
-app.use(bodyParser());
+app.use(bodyParser.urlencoded({extended: false}));
 var port = process.env.PORT || 8080; // process.env.PORT lets heroku set the port.
 MongoClient = require('mongodb').MongoClient;
 
 var accountSid = 'AC9550722b43b00be70c63b686a2cc07c0';
 var authToken = '9a93918597cfc083f72a4f443b4420ef';
-var twilio = require('twilio');
+var twilio = require('twilio')(accountSid, authToken);
 //var client = new twilio.RestClient(accountSid, authToken);
 
 app.set('view engine', 'jade');
@@ -25,6 +25,7 @@ MongoClient.connect('mongodb://heroku_cxgp2vvm:caetrp2v57asq6a593ub7i7891@ds1492
 		});
 	};
 	var addAnime = function(req, res){
+		req.body.votes = 2;
 		collection.insert(req.body, function(err, docs){
 			console.log(docs);
 			res.redirect('/anime');
@@ -35,25 +36,30 @@ MongoClient.connect('mongodb://heroku_cxgp2vvm:caetrp2v57asq6a593ub7i7891@ds1492
 		res.redirect('/anime');
 	};
 	var voteSMS = function(req, res){
-		if(twilio.validateExpressRequest(req, authToken)){
-			res.header('Content-Type', 'text/xml');
-			var body = req.param('Body').trim();
-			var to = req.param('To'); // Number vote is sent to.
-			var from = req.param('From'); // Voter number.
-			var body = req.param('Body');
+		console.log(req.body) // Details about the incoming text are passed into the body of the request.
+		var textFrom = req.body.From;
+		var textBody = req.body.Body;
 
-			res.send('<Response><Sms>If you get this, then it means shit worked.</Sms></Response>'); 
-		}
+		res.send(`
+			<Response>
+				<Message>
+					Thanks! Your vote for ${textBody} has been recorded.
+				</Message>
+			</Response>
+		`);
 	}
 	var redir = function(req, res){
 		res.redirect('/anime');
 	}
 
-	app.get('/vote', index);
 	app.get('/', redir);
+	
+	app.get('/vote', index);
 	app.post('/vote', voteSMS);
+	
 	app.get('/anime', index);
 	app.post('/anime', addAnime);
+	
 	app.get('/reset', resetAnime);
 
 	app.listen(port);
